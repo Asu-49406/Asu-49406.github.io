d9f6ee0455c41aa216de4eba6eecff9b6846c264
Asu-49406 <63113616+Asu-49406@users.noreply.github.com>
2020-04-13 15:16:15 +0300

    Update imageblic.js

--- scripts/imageblic.js
+++ scripts/imageblic.js
@@ -2,9 +2,9 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/yoba1.png') {
-      myImage.setAttribute ('src','images/yoba2.png');
+    if(mySrc === 'https://sun9-33.userapi.com/c855132/v855132617/21d6e4/kO5Mynu_x84.jpg') {
+      myImage.setAttribute ('src','https://sun9-32.userapi.com/c858416/v858416607/1beec1/kPRBKLH-RU4.jpg');
     } else {
-      myImage.setAttribute ('src','images/yoba2.png');
+      myImage.setAttribute ('src','https://sun9-32.userapi.com/c858416/v858416607/1beec1/kPRBKLH-RU4.jpg');
     }
 }
